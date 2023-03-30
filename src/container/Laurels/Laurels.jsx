ed32096cb56809b85d9098b02385c658ad3adfab
DESKTOP-3IDEHA2\Zach <zmoeller0912@yahoo.com>
2023-03-29 19:40:09 -0500

    Added the Laurels and Gallery components

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,11 +1,35 @@
 import React from "react";
-import { images } from "../../constants";
+import { images, data } from "../../constants";
+import { SubHeading } from "../../components";
 import "./Laurels.css";
 
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
+  <div className="app__laurels_awards-card">
+    <img src={imgUrl} alt="award" />
+    <div className="app__laurels_awards-cards_content">
+      <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+        {title}
+      </p>
+      <p className="p__cormorant">{subtitle}</p>
+    </div>
+  </div>
+);
+
 const Laurels = () => (
-  <div className="app__Laurels">
-    <div className="app__logo">
-      <img src={images.logo} />
+  <div className="app__bg app__wrapper section__padding" id="awards">
+    <div className="app__wrapper_info">
+      <SubHeading title="Awards & Recognition" />
+      <h1 className="headtext__cormorant">Our Laurles</h1>
+
+      <div className="app__laurels_awards">
+        {data.awards.map((award) => (
+          <AwardCard award={award} key={award.title} />
+        ))}
+      </div>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.laurels} alt="Award Photo" />
     </div>
   </div>
 );
