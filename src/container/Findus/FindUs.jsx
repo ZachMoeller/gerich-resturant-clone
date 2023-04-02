f92f654ba3cc60867948e28d071d8d5425119ca0
DESKTOP-3IDEHA2\Zach <zmoeller0912@yahoo.com>
2023-04-02 18:12:06 -0500

    Added the FindUs Component

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import React from "react";
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
+import "./FindUs.css";
 
 const FindUs = () => (
-  <div>
-    FindUs
+  <div
+    className="app__findus app__bg app__wrapper section__padding"
+    id="contact"
+  >
+    <div className="app__findus-contact ">
+      <SubHeading title="Contact" />
+      <h1 className="headtext__cormorant">Find Us</h1>
+      <div className="app__findus-contact-address">
+        <p className="p__opensans">
+          Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 7SG
+        </p>
+      </div>
+      <div className="app__findus-contact_hours">
+        <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+          Opening Hours
+        </p>
+        <p className="p__opensans">Mon - Fri: 10:00 am - 02:00 am</p>
+        <p className="p__opensans">Sat - Sun: 10:00 am - 03:00 am</p>
+      </div>
+      <button type="button" className="custom__button">
+        Visit Us
+      </button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="Find Us" />
+    </div>
   </div>
 );
 
